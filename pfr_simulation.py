71c1e8f0c913ce502bd763c6309e8af7509231b2
adityasengar <adityasengariitd@gmail.com>
2021-08-15 10:00:00 +0200

    refactor: Improve code clarity and comments

--- pfr_simulation.py
+++ pfr_simulation.py
@@ -4,62 +4,90 @@ import argparse
 import os
 import matplotlib.pyplot as plt
 
-def F_x(x, params):
+def F_x(propylene_conc, params):
+    """
+    Calculates an intermediate term for the deactivation rate expression.
+    """
     k_d = params.get('k_d', 1.0)
     iC4_conc = params.get('iC4_conc', 1.0)
     x_k_T = params.get('x_k_T', 1.0)
-    return (k_d / iC4_conc) * (x_k_T + x)
+    return (k_d / iC4_conc) * (x_k_T + propylene_conc)
 
-def d_lambda_dt(lambda_val, x, params):
-    sum_C_i = x * 0.1 
-    return -F_x(x, params) * lambda_val * (sum_C_i / params.get('iC4_conc', 1.0))
+def d_lambda_dz(activity, propylene_conc, params):
+    """
+    Calculates the spatial derivative of the fraction of active sites (lambda).
+    This is a simplified model for catalyst deactivation along the reactor length.
+    d(lambda)/dz = -F_x * lambda * (C_oligomer / C_iC4)
+    """
+    # Assuming oligomer concentration is proportional to propylene concentration
+    oligomer_conc = propylene_conc * 0.1 
+    iC4_conc = params.get('iC4_conc', 1.0)
+    return -F_x(propylene_conc, params) * activity * (oligomer_conc / iC4_conc)
 
-def d_C_prime_dz(lambda_val, x, params):
+def d_propylene_conc_dz(activity, propylene_conc, params):
+    """
+    Calculates the spatial derivative of the normalized propylene concentration.
+    This is a simplified model for the reaction rate.
+    dx/dz = -k_reaction * lambda * x
+    """
     k_reaction = params.get('k_reaction', 10.0)
-    return k_reaction * lambda_val * x
+    return -k_reaction * activity * propylene_conc
 
 def run_pfr_simulation(params):
+    """
+    Runs the Plug Flow Reactor (PFR) simulation.
+    """
     num_segments = params.get('num_segments', 100)
     total_length = params.get('total_length', 1.0)
     dz = total_length / num_segments
+
+    # Initial conditions
     x0 = params.get('initial_x', 1.0)
     lambda0 = params.get('initial_lambda', 1.0)
+
+    # Profiles along the reactor
     z_profile = np.linspace(0, total_length, num_segments + 1)
-    x_profile = np.zeros(num_segments + 1)
-    lambda_profile = np.zeros(num_segments + 1)
-    x_profile[0] = x0
-    lambda_profile[0] = lambda0
+    propylene_profile = np.zeros(num_segments + 1)
+    activity_profile = np.zeros(num_segments + 1)
+    
+    propylene_profile[0] = x0
+    activity_profile[0] = lambda0
     
     print("Running PFR simulation...")
+    # Integrate along the reactor length using Euler's method
     for i in range(num_segments):
-        dx_dz = -d_C_prime_dz(lambda_profile[i], x_profile[i], params)
-        dlambda_dz = -d_lambda_dt(lambda_profile[i], x_profile[i], params) 
-        x_profile[i+1] = x_profile[i] + dx_dz * dz
-        lambda_profile[i+1] = lambda_profile[i] + dlambda_dz * dz
+        dx_dz = d_propylene_conc_dz(activity_profile[i], propylene_profile[i], params)
+        dlambda_dz = d_lambda_dz(activity_profile[i], propylene_profile[i], params) 
+        
+        propylene_profile[i+1] = propylene_profile[i] + dx_dz * dz
+        activity_profile[i+1] = activity_profile[i] + dlambda_dz * dz
         
     return pd.DataFrame({
         'reactor_length': z_profile,
-        'propylene_conc': x_profile,
-        'active_sites': lambda_profile
+        'propylene_concentration': propylene_profile,
+        'catalyst_activity': activity_profile
     })
 
 def plot_results(df, output_path):
+    """
+    Plots the simulation results.
+    """
     print(f"Generating plot to {output_path}...")
     fig, ax1 = plt.subplots(figsize=(10, 6))
     
     color = 'tab:red'
-    ax1.set_xlabel('Reactor Length (z\')')
-    ax1.set_ylabel('Propylene Concentration (x/x0)', color=color)
-    ax1.plot(df['reactor_length'], df['propylene_conc'], color=color)
+    ax1.set_xlabel("Reactor Length (z')")
+    ax1.set_ylabel('Normalized Propylene Concentration', color=color)
+    ax1.plot(df['reactor_length'], df['propylene_concentration'], color=color)
     ax1.tick_params(axis='y', labelcolor=color)
 
     ax2 = ax1.twinx()
     color = 'tab:blue'
     ax2.set_ylabel('Fraction of Active Sites (λ)', color=color)
-    ax2.plot(df['reactor_length'], df['active_sites'], color=color)
+    ax2.plot(df['reactor_length'], df['catalyst_activity'], color=color)
     ax2.tick_params(axis='y', labelcolor=color)
 
-    plt.title('PFR Concentration and Active Site Profiles')
+    plt.title('PFR Concentration and Activity Profiles')
     fig.tight_layout()
     plt.savefig(output_path)
     print(f"Plot saved to {output_path}")
